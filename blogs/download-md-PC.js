23531ec2b7de1b3cd7945434b50c61856c5908cb
Michael An <2331806369@qq.com>
2023-12-01 23:39:52 +0800

    update docs

---
+++ blogs/download-md-PC.js
@@ -0,0 +1,36 @@
+function getResuleFromTable() {
+  const title = document.querySelectorAll('.huVRfc')[0].innerText;
+  const contentText = document.querySelectorAll('.charcoal-token')[1].innerText;
+  const tags = document.querySelectorAll('.iWBYKe');
+  const tagsContent = [...tags].map(tag => {
+    return tag.innerText;
+  }).join(', ');
+  let content = `
+  ## ${title}
+
+  #### 标签 ${tagsContent}
+
+  原文链接 ${location.href}
+  \n
+  ${contentText}
+    `;
+  content = content.replace(/\n/ig, '\n\n');
+  return { title, content };
+}
+
+
+function downLoadResult(fileName, resultStr) {
+  const blob = new Blob([resultStr], {
+    type: "text/plain;charset=utf-8"
+  });
+  const objectURL = URL.createObjectURL(blob);
+  const aTag = document.createElement('a');
+  aTag.href = objectURL;
+  aTag.download = fileName + ".md";
+  aTag.click();
+  URL.revokeObjectURL(objectURL);
+}
+
+const { title, content } = getResuleFromTable();
+
+downLoadResult(title, content);
